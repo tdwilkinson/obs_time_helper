592d7fb14debacc42aef52281fbfd06876aa6eb8
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-06-16 01:21:16 -0700

    correctly adjusts now

--- obs_time_helper/exptime_calc.py
+++ obs_time_helper/exptime_calc.py
@@ -12,14 +12,15 @@ class by_mag_comparison():
         # want to incorporate some kind of sn adjustment
         # ratio_sn = ref_sn / target_sn
         # print 'sn ratio' , ratio_sn
-        exptime = (ref_sn * ref_exptime) / target_sn
-        adj_time = exptime + ref_exptime
+        exptime = (target_sn * ref_exptime) / ref_sn
+        print 't:', exptime
 
         # use ref_magnitude and array_magnitudes to do a comparison
         for mag in array_magnitudes:
             ratio_flux = 10. ** ((- ref_magnitude) / 2.5) / 10. ** ((-mag) / 2.5)
             # m1/m2 = t2/t1 >> t2 =  m1/m2 * t1
-            new_exptime = ratio_flux * adj_time
+            new_exptime = ratio_flux * exptime
+
             print mag, new_exptime
 
         print 'mag 14.9 expect ~ 661.441 s'
