c733f1a94c1fef44b0a45f8e51f0f610cf728d29
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-06-16 03:39:07 -0700

    final edits

--- obs_time_helper/exptime_calc.py
+++ obs_time_helper/exptime_calc.py
@@ -6,14 +6,8 @@ import numpy as np
 class by_mag_comparison():
     def __init__(self, ref_magnitude, ref_sn, ref_exptime, target_sn, array_magnitudes):
 
-
-        gain = 1.88 # needed?
-
-        # want to incorporate some kind of sn adjustment
-        # ratio_sn = ref_sn / target_sn
-        # print 'sn ratio' , ratio_sn
         exptime = (target_sn * ref_exptime) / ref_sn
-        print 't:', exptime
+        print 'recommended exp time:', exptime
 
         # use ref_magnitude and array_magnitudes to do a comparison
         for mag in array_magnitudes:
@@ -23,5 +17,4 @@ class by_mag_comparison():
 
             print mag, new_exptime
 
-        print 'mag 14.9 expect ~ 661.441 s'
 
