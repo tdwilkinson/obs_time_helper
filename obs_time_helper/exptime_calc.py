5c611d7598b436ef0cecde1086ac49c893a462cc
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-06-15 23:12:01 -0700

    to help adjust exposure times during observation

---
+++ obs_time_helper/exptime_calc.py
@@ -0,0 +1,26 @@
+__author__ = 'tdwilkinson'
+
+import numpy as np
+
+
+class by_mag_comparison():
+    def __init__(self, ref_magnitude, ref_sn, ref_exptime, target_sn, array_magnitudes):
+
+
+        gain = 1.88 # needed?
+
+        # want to incorporate some kind of sn adjustment
+        # ratio_sn = ref_sn / target_sn
+        # print 'sn ratio' , ratio_sn
+        exptime = (ref_sn * ref_exptime) / target_sn
+        adj_time = exptime + ref_exptime
+
+        # use ref_magnitude and array_magnitudes to do a comparison
+        for mag in array_magnitudes:
+            ratio_flux = 10. ** ((- ref_magnitude) / 2.5) / 10. ** ((-mag) / 2.5)
+            # m1/m2 = t2/t1 >> t2 =  m1/m2 * t1
+            new_exptime = ratio_flux * adj_time
+            print mag, new_exptime
+
+        print 'mag 14.9 expect ~ 661.441 s'
+
